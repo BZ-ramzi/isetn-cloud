9ea05b7df9472b9e36e79e2cafe8599e671e7d7c
Ramzi BENZAID <ramzibenzaeid@gmail.com>
2017-06-11 10:15:49 +0200

    b

--- Connections/isetn.php
+++ Connections/isetn.php
@@ -6,5 +6,6 @@ $hostname_isetn = "localhost";
 $database_isetn = "isetn";
 $username_isetn = "root";
 $password_isetn = "";
-$isetn = mysql_pconnect($hostname_isetn, $username_isetn, $password_isetn) or trigger_error(mysql_error(),E_USER_ERROR); 
+
+$isetn = new PDO('mysql:host='.$hostname_isetn.';dbname='.$database_isetn.', '.$username_isetn.', '.$password_isetn.'');
 ?>
